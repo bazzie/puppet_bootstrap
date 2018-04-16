da16843ab9295e298da2926d8599e6057642e75c
Bas Wildemann <bas@MacBook-Pro-van-Bas.local>
2018-04-16 20:55:56 +0200

    bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 yum -y localinstall https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
 yum -y install puppet-agent
-
+source /etc/profile.d/puppet-agent.sh
 puppet module install camptocamp-puppetserver --version 2.1.0
 puppet module install puppetlabs-puppetdb --version 6.0.2
 
